6f54e474c26d6f78b325180807f9a4df99c98033
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-08-05 21:41:34 +0600

    init: websockets

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
-import WSServer from "express-ws"
+import expressWs from "express-ws"
+import { Application } from 'express';
 
-const app = express()
+const { app } = expressWs(express() as Application);
 const PORT = process.env.PORT || 5000
 
+expressWs(app)
+
 app.listen(PORT, ()=> console.log(`Starting on port ${PORT}`))
+
+app.ws('/', (ws, req) => {
+    console.log('Client connected');
+
+});
